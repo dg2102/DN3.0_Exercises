7f6bd037fa47b2c879ce5cc21b5162e206c2017e
Debparna Ghatak <141862198+dg2102@users.noreply.github.com>
2024-08-08 02:12:36 +0530

    Delete Week 1 directory

--- Week 1/Exercise 1/AdapterPatternExample/src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        PaymentProcessor paytmProcessor = new Paytmadapter();
-        paytmProcessor.processPayment(100);
-
-        PaymentProcessor gPaymentProcessor = new GpayAdapter();
-        gPaymentProcessor.processPayment(200);
-    }
-}
--- Week 1/Exercise 1/FactoryMethodPattern/src/WordDocFactory.java
+++
@@ -1,6 +0,0 @@
-public class WordDocFactory extends DocumentFactory  {
-    @Override
-    public Document createDocument() {
-        return new WordDoc();
-    }
-}
